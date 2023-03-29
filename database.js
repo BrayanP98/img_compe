50faa19c3b7d010477fc7a3e5c41d1bd182ead26
Brayan Perafan <perafanbrayan1998@gmail.com>
2023-03-29 11:31:35 -0500

    Update database.js

--- database.js
+++ database.js
@@ -7,8 +7,8 @@ const { connect }=require('mongoose');
      try{
 
 
-// const db =await connect("mongodb+srv://root:brayan98@cluster0.vbjahf5.mongodb.net/?retryWrites=true&w=majority");
-   const db =await connect("mongodb://localhost/usuarios");
+const db =await connect("mongodb+srv://root:8L1GYG6Olx9fz3tJ@cluster0.vbjahf5.mongodb.net/?retryWrites=true&w=majority");
+ //  const db =await connect("mongodb://localhost/usuarios");
 
         console.log("db connected");
 
