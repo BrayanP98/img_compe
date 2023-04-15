37d75bc859083bf4a03da91757c394bc6827b205
Brayan Perafan <perafanbrayan1998@gmail.com>
2023-04-14 20:54:52 -0500

    Update database.js

--- database.js
+++ database.js
@@ -2,23 +2,12 @@ const { connect }=require('mongoose');
 
 
 (async() => {
-    
-
      try{
-
-
-    //  const db =await connect( "mongodb://mongo:fweEbYTfnZTEU5gjnM0Q@containers-us-west-141.railway.app:7794");
-
-
-const db =await connect( "mongodb://mongo:fweEbYTfnZTEU5gjnM0Q@containers-us-west-141.railway.app:7794");
-  
         //    const db =await connect("mongodb://localhost/usuarios");
 
   // const db =await connect( "mongodb://mongo:fweEbYTfnZTEU5gjnM0Q@containers-us-west-141.railway.app:7794");
   
-       //const db =await connect("mongodb://localhost/db_competidor");
-
-
+     const db =await connect("mongodb://localhost/db_competidor");
         console.log("db connected");
 
      }catch(error){
