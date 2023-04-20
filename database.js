96b43a8a952f19267e740e038b06495c865a1002
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-04-20 12:55:34 -0500

    add database file

--- database.js
+++ database.js
@@ -6,19 +6,17 @@ const { connect }=require('mongoose');
 
 
 
-    //  const db =await connect( "mongodb://mongo:fweEbYTfnZTEU5gjnM0Q@containers-us-west-141.railway.app:7794");
+  
 
 
 //const db =await connect( "mongodb://mongo:fweEbYTfnZTEU5gjnM0Q@containers-us-west-141.railway.app:7794");
   
         //  const db =await connect("mongodb://localhost/usuarios");
 
-        //    const db =await connect("mongodb://localhost/usuarios");
+      //  const db =await connect("mongodb://localhost/usuarios");
 
-
-  // const db =await connect( "mongodb://mongo:fweEbYTfnZTEU5gjnM0Q@containers-us-west-141.railway.app:7794");
   
-    const db =await connect("mongodb://localhost/db_competidor");
+  const db =await connect("mongodb://localhost/db_competidor");
         console.log("db connected");
 
      }catch(error){
